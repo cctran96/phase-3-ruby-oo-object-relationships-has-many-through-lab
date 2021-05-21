90a46bf985532aaa1b11c7e1b310c9b49046723a
Chau Tran <cctran96@gmail.com>
2021-05-20 21:22:19 -0700

    commit tree 2aff710a40b859f54e94bf6481b0babec12148fb

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,14 @@
+class Appointment 
+    @@all = []
 
+    def self.all
+        @@all
+    end
+
+    attr_accessor :date, :patient, :doctor
+
+    def initialize(date, patient, doctor)
+        @date, @patient, @doctor = date, patient, doctor
+        @@all << self
+    end
+end
--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,14 @@
+class Song
+    @@all = []
 
+    def self.all
+        @@all
+    end
+
+    attr_accessor :name, :artist, :genre
+
+    def initialize(name, artist, genre)
+        @name, @artist, @genre = name, artist, genre
+        @@all << self
+    end
+end
